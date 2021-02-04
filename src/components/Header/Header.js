43a4e24f9613f8b664075b5987c0c004da3f346c
Al Amin Khan <alaminkhan2010@gmail.com>
2021-02-04 19:36:30 +0600

    some fixes to header components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import logo from '../../images/logo2.png';
 import './Header.css'
+import { FiShoppingCart } from '@react-icons/all-files/fi/FiShoppingCart';
 
 const Header = () => {
   return (
@@ -20,9 +21,11 @@ const Header = () => {
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="ml-auto">
-            <Nav.Link href="#login" className='text-dark'>Login</Nav.Link>
-            <Nav.Link href="#signup" className='rounded-pill bg-pink text-white pl-pr'>Sign Up</Nav.Link>
+          <Nav className="ms-auto pe-4">
+            <Nav.Link href="#login" className='text-dark me-3'><FiShoppingCart/></Nav.Link>
+            <Nav.Link href="#login" className='text-dark me-3'>Login</Nav.Link>
+            <Nav.Link href="#signup" className='rounded-pill bg-pink text-white me-3 pl-pr'>Sign Up</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Navbar>
